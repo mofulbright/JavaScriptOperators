ae7ffc85a6e4aded2c2dc5f084827d9204913563
mofulbright <mofulbright02@gmail.com>
2022-04-26 11:04:01 -0300

    initial

---
+++ app.js
@@ -0,0 +1,41 @@
+//Exercise 1
+var a = 20; 
+var b = 4;
+var add = a + b;
+var minus = a - b;
+var multiply = a * b;
+var dividing = a / b;
+console.log(add);
+console.log(minus);
+console.log(multiply);
+console.log(dividing);
+
+//Exercise 2
+let x = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
+console.log(x + str);
+console.log(x + str2);
+console.log(x + isPresent);
+console.log(x + firstName);
+console.log(x + lastName);
+
+//Exercise 3
+let d = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = "false";
+let firstName2 = "Robin";
+let lastName2 = "Williams";
+console.log(d == str3); // true
+console.log(d === str3);//false
+console.log(!isPresent2);//true
+console.log("eleven" == str4 && d >= str3);//false
+console.log(!isPresent2 || isPresent2);//false
+console.log(0 == false);//true
+console.log(0 === false);//false
+console.log(0 != false);//false
+console.log(0 !== false);//true
